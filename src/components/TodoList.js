dd469f815a4050d664123a37378661fab3eea05f
TACULMA2 <marigold.taculog@oocl.com>
2023-08-29 00:00:19 +0800

    feat: implement console.warn

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,12 @@ import TodoGroup from './TodoGroup';
 const TodoList = ({ lists }) => {
   return (
     <div>
-      {lists.map((list) => (
-        <TodoGroup key={list.id} list={list} />
-      ))}
+      {lists.map((list, index) => {
+        if (!list.id) {
+          console.warn(`List at index ${index} does not have a unique "key" prop.`);
+        }
+        return <TodoGroup key={list.id} list={list} />;
+      })}
     </div>
   );
 };
